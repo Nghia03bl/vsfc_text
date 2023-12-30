1d4df5cbd16454d41f0110c81420b62c738c0f1f
Nghia03bl <117901265+Nghia03bl@users.noreply.github.com>
2023-12-30 11:00:23 +0700

    Update main.py

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ class_list = {'0': 'Negative', '1': 'Neutral', '2': 'Positve'}
 
 st.title('Sentiment analysis from Vietnamese students’ feedback')
 
-image = Image.open('feedback.jpg')
+image = Image.open('feedback.JPG')
 st.image(image)
 
 input_ec = open('ec_vsfc.pkl', 'rb')
